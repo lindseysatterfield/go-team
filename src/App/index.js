1467a6b41bd0e5184ca65b0e94589f41f95f0b65
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-05-05 19:27:32 -0500

    setting state and effect for user

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Routes from '../helpers/Routes';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((authedUser) => {
+      if (authedUser) {
+        const userInfoObject = {
+          fullName: authedUser.displayName,
+          profileImage: authedUser.photoURL,
+          uid: authedUser.uid,
+          user: authedUser.email.split('@')[0]
+        };
+        setUser(userInfoObject);
+      } else if (user || user === null) {
+        setUser(false);
+      }
+    });
+  }, []);
+
   return (
     <div className='App'>
       <Router>
